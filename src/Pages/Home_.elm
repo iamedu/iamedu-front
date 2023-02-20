0220a3c198f10b457c450c13644aa17696cecf83
Eduardo Diaz <me@iamedu.email>
2023-02-19 22:05:29 -0800

    Add tailwind

--- src/Pages/Home_.elm
+++ src/Pages/Home_.elm
@@ -1,11 +1,16 @@
 module Pages.Home_ exposing (page)
 
 import Html
+import Html.Attributes exposing (class)
 import View exposing (View)
 
 
 page : View msg
 page =
     { title = "Homepage"
-    , body = [ Html.text "Hello, world!" ]
+    , body =
+        [ Html.h1
+            [ class "text-3xl font-bold underline" ]
+            [ Html.text "Hello, world!" ]
+        ]
     }
