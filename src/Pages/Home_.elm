420717f4f013654b30e3b98b8935e8643678c96f
Eduardo Diaz <me@iamedu.email>
2023-02-22 20:21:25 -0800

    Elm stuff

--- src/Pages/Home_.elm
+++ src/Pages/Home_.elm
@@ -1,7 +1,7 @@
 module Pages.Home_ exposing (page)
 
-import Html
-import Html.Attributes exposing (class)
+import Html exposing (..)
+import Html.Attributes exposing (class, href, src)
 import View exposing (View)
 
 
@@ -9,8 +9,26 @@ page : View msg
 page =
     { title = "Homepage"
     , body =
-        [ Html.h1
-            [ class "text-3xl font-bold underline" ]
-            [ Html.text "Hello, world!" ]
+        [ div [ class "container mx-auto" ]
+            [ div
+                [ class "flex items-center justify-between py-6 lg:py-10" ]
+                [ a
+                    [ href "/", class "flex items-center" ]
+                    [ span
+                        [ class "mr-2" ]
+                        [ img [ src "img/logo.svg" ] [] ]
+                    , p
+                        [ class "hidden font-body text-2xl font-bold text-primary dark:text-white lg:block" ]
+                        [ text "iamedu" ]
+                    ]
+                ]
+            ]
+        , div [ class "container mx-auto" ]
+            [ div [ class "border-b border-grey-lighter py-16 lg:py-20" ]
+                [ h1
+                    [ class "pt-3 font-body text-4xl font-semibold text-primary dark:text-white md:text-5xl lg:text-6xl" ]
+                    [ text "Hi, I am Edu" ]
+                ]
+            ]
         ]
     }
